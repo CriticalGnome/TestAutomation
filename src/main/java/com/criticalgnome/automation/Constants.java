7cf71752b2c82ce519f133b0026c34417befc76c
Sergey Kalinovsky <lord.skiminok@gmail.com>
2017-10-17 12:32:49 +0300

    Test commit

--- src/main/java/com/criticalgnome/automation/Constants.java
+++ src/main/java/com/criticalgnome/automation/Constants.java
@@ -14,8 +14,8 @@ public enum Constants {
     public static final String SEMICOLON = "';'";
     public static final String QUOTATION = "'";
     public static final String DOUBLE_QUOTATION = "\"";
-    public static final String ABRACADABRA1 = "AbRaCaDaBrA";
-    public static final String ABRACADABRA2 = "aBrAcAdAbRa";
+    public static final String ABRACADABRA1 = "AbRaCaDaBrA1";
+    public static final String ABRACADABRA2 = "aBrAcAdAbRa1";
     public static final String FEATURES_PAGE_LINK = "https://github.com/features";
     public static final String EXPLORE_PAGE_LINK = "https://github.com/explore";
     public static final String PRICING_PAGE_LINK = "https://github.com/pricing";
